5ada3a75723f67c0f3d1221a06f75802bc76f30d
leonardoperu <38179736+leonardoperu@users.noreply.github.com>
2018-06-12 12:32:51 +0200

    Add files via upload

---
+++ Model/CalendarioImpegni.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelCtrlLake
+{
+    public class CalendarioImpegni
+    {
+        private List<Impegno> _impegni { get; }
+
+        public CalendarioImpegni()
+        {
+            this._impegni = new List<Impegno>();
+        }
+
+        public List<Impegno> GetImpegni ()
+        {
+            return this._impegni;
+        }
+
+        public Impegno getImpegno (DateTime inizio, DateTime fine)
+        {
+            Impegno res = null, i2=new Impegno(inizio, fine);
+            
+            foreach (Impegno i in this._impegni)
+                if (i.Equals(i2))
+                {
+                    res = i; break;
+                }
+            return res;
+        }
+
+        public void Aggiungi(DateTime inizio, DateTime fine)
+        {
+            Impegno imp = null;
+            try { imp = new Impegno(inizio, fine); }
+            catch (Exception e) { throw e; }
+            bool overlaps = false;
+            foreach (Impegno i in this._impegni)
+                if (i.OverlapsWith(imp))
+                {
+                    overlaps = true; break;
+                }
+            if (!overlaps)
+                this._impegni.Add(imp);
+            else
+                throw new Exception("L'impegno richiesto si sovrappone con uno già esistente!");
+        }
+
+        public void Rimuovi(DateTime inizio, DateTime fine)
+        {
+            Impegno imp = null;
+            imp = this.getImpegno(inizio, fine);
+            if (!this._impegni.Remove(imp))
+                throw new Exception("Impegno non presente in lista!");
+        }
+
+
+    }
+
+}
---
+++ Model/Impegno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelCtrlLake
+{
+     public class Impegno
+    {
+
+        private DateTime _inizio { get; set; }
+        private DateTime _fine { get; set; }
+
+        public Impegno(DateTime inizio, DateTime fine)
+        {
+            if (inizio.CompareTo(fine) >= 0)
+                throw new Exception("Impossibile creare impegno: intervallo non valido");
+            if(inizio.TimeOfDay.CompareTo(new TimeSpan(9,0,0))<0
+                || fine.TimeOfDay.CompareTo(new TimeSpan(19,0,0))>0)
+                throw new Exception("Impossibile creare impegno: orari non possibili");
+            if (!inizio.Date.Equals(fine.Date))
+                throw new Exception("Impossibile creare impegno: non ammessi inizio e fine in due giorni distinti");
+            this._inizio = inizio;
+            this._fine = fine;
+        }
+
+        public bool OverlapsWith(Impegno i2)
+        {
+            DateTime max_inizio = DateTime.Compare(this._inizio, i2._inizio) >= 0 ? this._inizio : i2._inizio;
+            DateTime min_fine = DateTime.Compare(this._fine, i2._fine) <= 0 ? this._fine : i2._fine;
+            if (DateTime.Compare(max_inizio, min_fine) < 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool Equals(Impegno i2)
+        {
+            bool result = ( this._inizio.CompareTo(i2._inizio) == 0
+                && this._fine.CompareTo(i2._fine) == 0 );
+            return result;
+        }
+    }
+}
